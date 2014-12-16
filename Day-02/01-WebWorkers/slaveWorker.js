201c5e938053a4a199f103a7bc501454ca3a6098
tkmagesh <tkmagesh77@gmail.com>
2014-12-16 15:07:47 +0530

    Server sent events

--- Day-02/01-WebWorkers/slaveWorker.js
+++ Day-02/01-WebWorkers/slaveWorker.js
@@ -1,13 +1,20 @@
 function doWork(){
-	for(var i=0;i<10000;i++)
+	for(var i=0;i<10000;i++){
 		for(var j=0;j<10000;j++)
 			for(var k=0;k<100;k++){}
+		if (i % 100 === 0){
+			self.postMessage({
+				type : "progress",
+				percentCompleted : (i/10000)*100
+			});
+		}
+	}
 }
 self.addEventListener("message", function(msgEvt){
 	if (msgEvt.data === "start"){
 		doWork();
 		console.log("Worker task completed");
-		self.postMessage("done");		
+		self.postMessage({type : "done"});		
 	} else {
 		console.log("Unknown message from main thread");
 	}
