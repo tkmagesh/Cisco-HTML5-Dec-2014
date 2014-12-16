3c1174e87e90a8dc7f71cf70b0e73c8aac88824b
tkmagesh <tkmagesh77@gmail.com>
2014-12-16 11:31:11 +0530

    Web Workers

---
+++ Day-02/01-WebWorkers/slaveWorker.js
@@ -0,0 +1,15 @@
+function doWork(){
+	for(var i=0;i<10000;i++)
+		for(var j=0;j<10000;j++)
+			for(var k=0;k<100;k++){}
+}
+self.addEventListener("message", function(msgEvt){
+	if (msgEvt.data === "start"){
+		doWork();
+		console.log("Worker task completed");
+		self.postMessage("done");		
+	} else {
+		console.log("Unknown message from main thread");
+	}
+});
+
